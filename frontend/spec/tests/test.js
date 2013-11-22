ab140a0c97adf323d0e6617c2dfdb35319438082
Doug Kumagai <doug@mapletonhillmedia.com>
2013-11-22 12:12:09 -0700

    breaking a test

--- frontend/spec/tests/test.js
+++ frontend/spec/tests/test.js
@@ -3,6 +3,7 @@ describe("Array", function(){
 		it("should return -1 when the value is not present", function(){
 			assert(-1, [1,2,3].indexOf(5));
 			assert(-1, [1,2,3].indexOf(0));
+			assert(false);
 		});
 	});
 });
